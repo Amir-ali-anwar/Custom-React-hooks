1cccdec640420b787531da801694c6e393f8929e
Amir Ali anwar <amiralianwar611@gmail.com>
2023-05-10 16:57:08 +0500

    Fixing the issues

--- src/useToggle/ToggleComponent.tsx
+++ src/useToggle/ToggleComponent.tsx
@@ -5,8 +5,8 @@ const [toggle,handleToggle]= useToggle({initialValue:false})
   return (
     <div>
       <button onClick={handleToggle}> Toggle {toggle} </button>
-      <button onClick={() => handleToggle(true)}>Make True</button>
-      <button onClick={() => handleToggle(false)}>Make False</button>
+      <button onClick={() => handleToggle({toggle:true})}>Make True</button>
+      <button onClick={() => handleToggle({toggle:false})}>Make False</button>
     </div>
   )
 }
--- src/useToggle/useToggle.tsx
+++ src/useToggle/useToggle.tsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 
 type toggleProps = {
   initialValue: boolean;
 };
 const useToggle = ({ initialValue }: toggleProps) => {
-  const [toggle, SetToggle] = useState<boolean>(initialValue);
-  const handleToggle = (toggle: boolean): void => {
-    if(typeof toggle !== "boolean") throw Error("Please enter boolean value")
+  const [toggle, SetToggle] = useState<ReactNode>(initialValue);
+  console.log(toggle);
+    const handleToggle = (toggle: boolean): void => {
     SetToggle((currentValue) =>
       typeof toggle === "boolean" ? toggle : !currentValue
     );
